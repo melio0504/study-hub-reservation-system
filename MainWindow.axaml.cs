d82d985d0f39fbecb24bafbabda6bc28629a75cd
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-03-23 15:01:56 +0800

    Create initial project structure

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace study_hub_reservation_system;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
