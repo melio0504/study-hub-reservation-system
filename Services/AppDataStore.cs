d6c212c5091682b75254d6d5c8ae5440bcddffda
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-03-25 13:03:44 +0800

    Create initial component for AppDataStore

---
+++ Services/AppDataStore.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Text.Json;
+using study_hub_reservation_system.Models;
+namespace study_hub_reservation_system.Services;
+public class AppDataStore
+{
+}
