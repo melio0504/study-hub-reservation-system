c44de22790b0435e4c441a5b69edcd54b171c69f
melio0504 <82772382+melio0504@users.noreply.github.com>
2026-03-25 13:04:07 +0800

    Initialize private variables in AppDataStore

--- Services/AppDataStore.cs
+++ Services/AppDataStore.cs
@@ -6,7 +6,13 @@ using System.Security.Cryptography;
 using System.Text;
 using System.Text.Json;
 using study_hub_reservation_system.Models;
+
 namespace study_hub_reservation_system.Services;
+
 public class AppDataStore
 {
+    private readonly string _usersPath;
+    private readonly string _reservationsPath;
+    private readonly JsonSerializerOptions _jsonOptions;
+
 }
